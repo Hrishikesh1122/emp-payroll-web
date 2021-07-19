dbb8ec442f20aa55ce60a9e3bc22ffd6e0da979a
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-19 15:45:01 +0530

    added : functionality to reset button

--- Javascript/script.js
+++ Javascript/script.js
@@ -128,3 +128,35 @@ function createLocalStorage(employee){
     alert(empPayrollLocal.toString());
     localStorage.setItem("empListLocal",JSON.stringify(empPayrollLocal));
 }
+
+//Function to reset the form to original state
+const resetForm = () => {
+    setValue('#name', '');
+    unSetSelectedValues('[name=profile]');
+    unSetSelectedValues('[name=gender]');
+    unSetSelectedValues('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setTextValue('#salary-op', '400000');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+    
+}
+
+const unSetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
