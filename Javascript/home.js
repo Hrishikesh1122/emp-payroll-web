6287833ab4fc8bae41aac6bf22b86b890fcc7d2f
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-19 22:08:50 +0530

    added : uc5 day45 display using json object

--- Javascript/home.js
+++ Javascript/home.js
@@ -5,37 +5,72 @@ Purpose :- JavaScript functions for home page.
 @Since :- 19/07/2021
 */
 
+/*
+JSON Object for employees
+*/
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [{
+            _name: 'Rohan',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '50000',
+            _startDate: '29 Oct 2019',
+            _notes: '',
+            _id: new Date().getTime(),
+            _profile: '../assets/profile-images/Ellipse -1.png'
+        },
+        {
+            _name: 'Julia',
+            _gender: 'female',
+            _department: [
+                'Sales',
+            ],
+            _salary: '40000',
+            _startDate: '29 Oct 2019',
+            _notes: '',
+            _id: new Date().getTime(),
+            _profile: '../assets/profile-images/Ellipse -8.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
     createInnerHtml();
 });
-
-//Use of string literals to create inner Html
 const createInnerHtml = () => {
-    const innerHtml = `     
-    <tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>StartDate</th>
-    <th>Actions</th>
-</tr>
-<tr>
-    <td>
-        <img src="../assets/profile-images/Ellipse -2.png" class="profile" alt="">
-    </td>
-    <td>Hrishi</td>
-    <td>Male</td>
-    <td><div class="dept-label">Sales</div>
-    <div class="dept-label">Finance</div></td>
-    <td>300000</td>
-    <td>1 Nov 2020</td>
-    <td>
-        <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-        <img src="../assets/icons/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
-    </td>
-</tr>`;
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" src="${empPayrollData._profile}" alt=""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)" 
+                    src="../assets/icons/delete-black-18dp.svg" alt="delete">
+                <img name="${empPayrollData._id}" onclick="update(this)"
+                    src="../assets/icons/create-black-18dp.svg" alt="edit">
+            </td> 
+        </tr>
+        `;
+    }
+    document.querySelector('#display').innerHTML = innerHtml;
+}
 
-document.querySelector('#display').innerHTML = innerHtml;
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
+    }
+    return deptHtml;
 }
---
+++ Javascript/stringliterals.js
@@ -0,0 +1,41 @@
+/*
+Purpose :- Display table using string literals.
+Note :- This file is currently not in use for home.html as JSON object is used
+@author :- Hrishikesh
+@Version :- 1.0
+@Since :- 19/07/2021
+*/
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHtml();
+});
+
+//Use of string literals to create inner Html
+const createInnerHtml = () => {
+    const innerHtml = `     
+    <tr>
+    <th></th>
+    <th>Name</th>
+    <th>Gender</th>
+    <th>Department</th>
+    <th>Salary</th>
+    <th>StartDate</th>
+    <th>Actions</th>
+</tr>
+<tr>
+    <td>
+        <img src="../assets/profile-images/Ellipse -2.png" class="profile" alt="">
+    </td>
+    <td>Hrishi</td>
+    <td>Male</td>
+    <td><div class="dept-label">Sales</div>
+    <div class="dept-label">Finance</div></td>
+    <td>300000</td>
+    <td>1 Nov 2020</td>
+    <td>
+        <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
+        <img src="../assets/icons/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+    </td>
+</tr>`;
+
+document.querySelector('#display').innerHTML = innerHtml;
+}
